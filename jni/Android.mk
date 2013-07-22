466f5a23c6f757f828aebea5849e80b6fd3d90c1
awol2010ex <awol2010ex@gmail.com>
2013-07-22 10:01:31 +0800

    升级到2.1.4e

--- jni/Android.mk
+++ jni/Android.mk
@@ -19,11 +19,19 @@ LOCAL_SRC_FILES := cnfcpp/main.cpp \
                    
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../Classes                   
 
-LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static cocos_lua_static
-            
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+LOCAL_WHOLE_STATIC_LIBRARIES += chipmunk_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_lua_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static                
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,CocosDenshion/android) \
-$(call import-module,cocos2dx) \
-$(call import-module,scripting/lua/proj.android) \
+
+$(call import-module,CocosDenshion/android)
+$(call import-module,cocos2dx)
+$(call import-module,cocos2dx/platform/third_party/android/prebuilt/libcurl)
 $(call import-module,extensions)
+$(call import-module,scripting/lua/proj.android)
+$(call import-module,external/Box2D)
+$(call import-module,external/chipmunk)
